06a595f03f80b5ed2926ce4fac138be2a49ddc27
ivanarganda <ivanartista96@gmail.com>
2025-09-29 09:26:52 +0200

    added maitennance class

---
+++ class_/Mantenimiento.py
@@ -0,0 +1,8 @@
+from class_.Vehicle import Vehicle
+
+class Maintenance(Vehicle):
+    def __init__(self):
+        super().__init__(self)
+        self.maintenances_history = {}
+
+    def register_maintenance():pass
